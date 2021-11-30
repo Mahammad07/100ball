e376876239cc2ed5a03db2a1c59cc2b2af2f5a94
Mahammad07 <94453965+Mahammad07@users.noreply.github.com>
2021-11-29 18:18:09 -0600

    first

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 11;
+            int count = 0;
+
+            if (number < 0)
+            {
+                Console.WriteLine("eded 0 dan boyuk olmalidir");
+            }
+            if (number == 1)
+            {
+                Console.WriteLine("1 ne sadedir nede murekkeb");
+            }
+            else
+                for(int i = 1; i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        count++;
+
+                    }
+                }
+            if (count > 2)
+            {
+                Console.WriteLine("murekkeb");
+            }
+            else
+            {
+                Console.WriteLine("sade");
+            }
+
+        }
+    }
+}
---
+++ reqemlerin cemiii/reqemlerin cemiii/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace reqemlerin_cemiii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 19;
+            int count = 0;
+            int result = 0;
+
+            if (number < 10)
+            {
+                Console.WriteLine("eded 10 ve ya 10 dan boyuk olmalidir");
+            }
+            else
+            {
+                for(int i = 0; i < 2; i++)
+                {
+                    result = number % 10;
+                    number /= 10;
+                    count += result;
+
+                }
+                
+
+            }
+
+            {
+                Console.WriteLine(count);
+            }
+
+        }
+    }
+}
